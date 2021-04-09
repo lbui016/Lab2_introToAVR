65a6f21f1163d91ae817e94c86d649b404daa995
Liam Bui <lbui016@wch136-24.cs.ucr.edu>
2021-04-08 21:01:08 -0700

    completed excercise 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
-/*	Author: lbui016
+/*	Author: Liam Bui
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
